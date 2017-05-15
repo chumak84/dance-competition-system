c0ab89cc65420292d70bc27b79350a2ac801e35e
Dmytro Chumachenko <chumachenko84@hotmail.com>
2017-05-15 03:20:49 -0400

    Added React, React Router, Webpack, setup dev environment, Add Header, Home and Add pages

---
+++ dcs-fe/src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Dance Competition System</h1>
+                <p>System will help to setup and handle the event</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
---
+++ dcs-fe/src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Dummy test', () => {
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+});
