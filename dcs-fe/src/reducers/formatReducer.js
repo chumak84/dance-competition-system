b4e81b2c2bdd03b0b3986321f5b444b1fc9c2792
chumak84 <chumachenko84@hotmail.com>
2017-07-05 00:50:38 -0400

    Added additonal actions to format Actions list

--- dcs-fe/src/reducers/formatReducer.js
+++ dcs-fe/src/reducers/formatReducer.js
@@ -4,11 +4,18 @@ export default function formatReducer(state = [], action) {
     switch(action.type) {
         case types.LOAD_FORMATS_SUCCESS:
             return action.formats;
-        case types.UPDATE_FORMAT_SUCCESS:
+        case types.CREATE_FORMAT_SUCCESS:
             return [
                 ...state,
                 Object.assign({}, action.format)
-            ]
+            ];
+        case types.UPDATE_FORMAT_SUCCESS:
+            let newState = [...state];
+            let index = newState.findIndex(f => f.id === action.format.id);
+            debugger;
+            if(index >= 0)
+                newState.splice(index, 1, action.format);
+            return newState;
         default:
             return state;
     }
