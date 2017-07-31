f1fef2c809090c3f777ccd654c4ed621285e0250
chumak84 <chumachenko84@hotmail.com>
2017-07-30 23:29:56 -0400

    Add support Formats API to Services close #6

---
+++ dcs-api/Controllers/FormatsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dcs_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FormatsController : Controller
+    {
+        private static readonly Dictionary<int, string> _formats = new Dictionary<int, string>
+        {
+            [1] = "Big Group",
+            [2] = "Small Group",
+            [3] = "Individual",
+            [4] = "Solo"
+        };
+
+        [HttpGet]
+        public IDictionary<int, string> Get()
+        {
+            return _formats;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _formats[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            _formats.Add(_formats.Keys.Max() + 1, value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            _formats[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _formats.Remove(id);
+        }
+    }
+}
--- dcs-api/Program.cs
+++ dcs-api/Program.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-namespace dcs_services
+namespace dcs_api
 {
     public class Program
     {
@@ -16,6 +17,7 @@ namespace dcs_services
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
